2773e34aeff29d2a851686a7a3e21d28cb6f573f
Veysel Hadzhiveysal <146268145+vhadzhiveysal@users.noreply.github.com>
2023-10-06 19:20:45 +0100

    finishing week 2 tasks
    continued from 209 - 211

--- Tasks/02 - Iteration/209-break-keyword/209-break-continue.cpp
+++ Tasks/02 - Iteration/209-break-keyword/209-break-continue.cpp
@@ -2,16 +2,23 @@
 
 int main()
 {
-    for (int i = 0; i == i; i++)   // potentially loop from 1 to 10  
+    bool earlyQuit = false;
+    for (int i = 0; i <= 10; i++)   // potentially loop from 1 to 10  
     {
         char c = getchar(); //Read character
         char _ = getchar(); //Read and discard newline (nice little hack :)
         if (c == 'q' || c == 'Q')
         {
             printf("\tUser stopped the loop early at %d\n", i);
+            earlyQuit = true;
             break;                  // break out of the for loop
         }
         printf("Loop counter = %d\n", i);
     }
+
+    if (earlyQuit == true) {
+        printf("Loop was exited early");
+    }
+
     puts("Done");
 }
--- Tasks/02 - Iteration/211-nested-loops/211-nested-loops.cpp
+++ Tasks/02 - Iteration/211-nested-loops/211-nested-loops.cpp
@@ -3,17 +3,20 @@
 int main()
 {
     // Outer loop 
-    for (int i = 1; i <= 2; i++)        // Outer loop executes twice
+    for (int i = 1; i <= 12; i++)        // Outer loop executes twice
     {
-        printf("Outer loop number %d\n", i);
+        printf("(%d times table)\n", i);
 
         // Inner loop
-        for (int j = 1; j <= 5; j++)    // Inner loop executes 5 times for each outer loop run so 10 time in total
+        for (int j = 1; j <= 12; j++)    // Inner loop executes 5 times for each outer loop run so 10 time in total
         {
-            printf("\tInner loop number %d\n", j);  // note the use of the tab (\t) option at the start to improve the layout
+            printf("\t%d x %d = %d\n", i,j,i*j);
             
-            //Task - Write another level of nesting here
-
         }
     }
 }
+/*
+task question : why cant i observe variable j during 2nd outer loop commencing at line 8
+answer: because after the inner for loop is done, variable j's memory gets removed, it's undefined now
+followup: it's also because variables inside loops are only visible inside the loop itself, when its outside it isn't visible
+*/
