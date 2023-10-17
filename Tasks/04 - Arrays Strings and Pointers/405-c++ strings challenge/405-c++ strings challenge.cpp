4f313c935793371b9cd6303f54a1de01cd12fd3d
Veysel Hadzhiveysal <146268145+vhadzhiveysal@users.noreply.github.com>
2023-10-17 16:26:31 +0100

    finished 405
    finished challenges 2 & 3 of c++ strings challenge

--- Tasks/04 - Arrays Strings and Pointers/405-c++ strings challenge/405-c++ strings challenge.cpp
+++ Tasks/04 - Arrays Strings and Pointers/405-c++ strings challenge/405-c++ strings challenge.cpp
@@ -30,9 +30,40 @@ int main()
     getline(std::cin, input);   //Read a line (users types this in)
     istringstream iss(input);   //Create a string "stream" (sequence of words)
     string word;                //This will hold the next word
+    int count = 0;
     while (iss >> word) {       //Read the next word (if there is one)
         cout << word << endl;   //Output each word in turn
+        if (word == "the") {
+            count += 1;
+        } else {
+            continue;
+        }
+    }
+    cout << "\"the\" is found " << count << " many times in that sentence" << endl;
+
+
+    // challenge 3 listed in the github file - reversing the elements of an array
+    // starting with an array of integers, write some code to reverse their order
+    // a. for the first version you can use a second array to hold result
+    // b. for second version, try to perform this using a single array (in-place modification)
+
+    int arr[] = { 1, 2, 3, 4, 5 };
+
+    int arrLength = sizeof(arr) / sizeof(int); //calculates length of array
+    
+    cout << "unreversed array:" << endl;
+
+    //write out elements of array
+    for (unsigned int n = 0; n < arrLength; n++) {
+        cout << arr[n] << " ";
     }
 
+    reverse(begin(arr), end(arr)); //reverses the array content using reverse function
+
+    cout << endl << "reversed array:" << endl;
+
+    for (unsigned int n = 0; n < arrLength; n++) {
+        cout << arr[n] << " ";
+    }
 }
 
