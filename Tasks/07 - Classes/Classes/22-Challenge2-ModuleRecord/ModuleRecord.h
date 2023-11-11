94ed0a1488b30fec288c65c3db6f5ee3c58b178d
Veysel Hadzhiveysal <146268145+vhadzhiveysal@users.noreply.github.com>
2023-11-11 19:22:19 +0000

    week 7 tasks up to 24-challenge3
    finish off challenge 3, and then thats it

--- Tasks/07 - Classes/Classes/22-Challenge2-ModuleRecord/ModuleRecord.h
+++ Tasks/07 - Classes/Classes/22-Challenge2-ModuleRecord/ModuleRecord.h
@@ -1,5 +1,35 @@
 #pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+
 class ModuleRecord
 {
+
+private:
+	string name;
+	string subjectArea = "COMP";
+	unsigned int code;
+	unsigned int semesterCredits = 20;
+	unsigned int semesters = 1;
+
+
+public:
+
+	ModuleRecord(string inputName, unsigned int inputCode) {
+		name = inputName;
+		code = inputCode;
+
+	}
+
+	void moduleSummary() {
+		cout << "Module Name: " << subjectArea << code << endl << "No# of semesters: " << semesters << endl << "Semester Credits: " << semesterCredits << endl;
+	}
+
+	void totalCredits() {
+		int total = semesterCredits * semesters;
+		cout << "Total number of credits: " << total << endl;
+	}
 };
 
---
+++ Tasks/07 - Classes/Classes/24-Challenge3-YearRecord/YearRecord.h
@@ -0,0 +1,10 @@
+#pragma once
+using namespace std;
+
+class YearRecord
+{
+private:
+	unsigned int studentNumber;
+	unsigned int moduleNumber;
+
+};
