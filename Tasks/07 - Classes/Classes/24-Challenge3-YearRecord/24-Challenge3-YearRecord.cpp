ac7655f853333d18b0f787fd81db4adbacfd6889
Veysel Hadzhiveysal <146268145+vhadzhiveysal@users.noreply.github.com>
2023-11-12 14:43:01 +0000

    updating challenge 3
    i decided to update challenge 3 to make it that it's user-based (in the way that the user chooses the member functions instead of the program itself being written to do that
    
    problem:
    in chooseOption(), it seems that updates to the YearRecord doesnt save
    
    for example:
    adding a new student name doesnt save it to the class, and studentNumber doesnt increase, instead the only thing it saves is when the class gets constructed, which is also based on user input too
    
    im not sure if its the chooseOption() function that's the problem or what
    
    the commented out code completely works fine, just as long as you comment out everything else instead
    (the chooseOption() function and the other code beneath the currently commented one)
    basically just invert what's commented and what's not

--- Tasks/07 - Classes/Classes/24-Challenge3-YearRecord/24-Challenge3-YearRecord.cpp
+++ Tasks/07 - Classes/Classes/24-Challenge3-YearRecord/24-Challenge3-YearRecord.cpp
@@ -2,14 +2,17 @@
 //
 
 #include <iostream>
+#include <string>
 #include "YearRecord.h"
 #include <vector>
 
 using namespace std;
 
+int chooseOption(YearRecord Year);
+
 int main()
 {
-	YearRecord Year1("Paul Paulington", "COMP1000");
+	/*YearRecord Year1("Paul Paulington", "COMP1000");
 
 	Year1.addModule("COMP1001");
 	Year1.addStudent("James Jameson");
@@ -21,6 +24,72 @@ int main()
 	Year1.checkStudent(studentName);
 	
 	studentName = "Jack Jackson";
-	Year1.checkStudent(studentName);
+	Year1.checkStudent(studentName);*/
 
+	string studentName, moduleName;
+
+	cout << "Create a YearRecord, inserting first student and first module." << endl 
+		<< "Enter student name:" << endl;
+	getline(cin, studentName);
+	
+	cout << endl << "Enter Module Name:" << endl;
+	getline(cin, moduleName);
+	
+	
+	YearRecord Year1(studentName, moduleName);
+
+	int value;
+
+	do {
+		value = chooseOption(Year1);
+		cout << endl;
+	} while (value != 5);
+
+
+
+	return 0;
+}
+
+int chooseOption(YearRecord Year) {
+	cout << "Choose Year Record options:" << endl
+		<< "1. Add student" << endl
+		<< "2. Add module" << endl
+		<< "3. Display Year Record" << endl
+		<< "4. Check student record" << endl
+		<< "5. Leave program" << endl;
+
+	string studentNamer, moduleNamer;
+
+	int choice;
+	cin >> choice;
+
+	cin.ignore();
+
+
+	switch (choice) {
+	case 1:
+		cout << "Enter student name:" << endl;
+		getline(cin, studentNamer);
+		Year.addStudent(studentNamer);
+		break;
+	case 2:
+		cout << "Enter module name:" << endl;
+		getline(cin, moduleNamer);
+		Year.addModule(moduleNamer);
+		break;
+	case 3:
+		Year.display();
+		break;
+	case 4:
+		cout << "Enter student name:" << endl;
+		getline(cin, studentNamer);
+		Year.checkStudent(studentNamer);
+		break;
+	case 5:
+		cout << "Leaving program";
+		break;
+	default:
+		break;
+	}
+	return choice;
 }
